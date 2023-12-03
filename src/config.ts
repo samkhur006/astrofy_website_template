90a90a10788b3747d13cfe371ba6e50f87ebe1c7
samkhur006 <sambhav_khurana@tamu.edu>
2023-12-02 21:09:02 -0600

    Site description updated

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Sambhav Khurana';
-export const SITE_DESCRIPTION = 'Personal Website.';
+export const SITE_DESCRIPTION = 'Take a stroll through my online digs and get to know the digital side of me!';
